2a0196cad2b34c699b93741d5dfaee952d313e12
Adil Jamal <jamal.adil7860@gmail.com>
2026-06-02 15:42:42 +0530

    While loop

---
+++ src/Loop/while_loop.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class while_loop {
+        public static void utility(int x) {
+            // code here
+            while (x >= 0){
+                System.out.print(x + " ");
+                x--;
+            }
+        }
+}
