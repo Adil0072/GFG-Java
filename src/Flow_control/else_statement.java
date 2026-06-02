b6874cf1860e5b1ccbbb09908e1169b2643473dd
Adil Jamal <jamal.adil7860@gmail.com>
2026-06-02 20:06:58 +0530

    else statement

---
+++ src/Flow_control/else_statement.java
@@ -0,0 +1,17 @@
+package Flow_control;
+import java.util.Scanner;
+
+public class else_statement {
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+
+            // code here
+            if (a > 100){
+                System.out.println("Big");
+            } else{
+                System.out.println("Small");
+            }
+        }
+}
+
