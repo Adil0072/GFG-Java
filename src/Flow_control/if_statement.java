c1dced68611a690ffd3fd5d3cf27562cb156bd64
Adil Jamal <jamal.adil7860@gmail.com>
2026-06-02 20:03:17 +0530

    if statement

---
+++ src/Flow_control/if_statement.java
@@ -0,0 +1,18 @@
+package Flow_control;
+import java.util.*;
+
+public class if_statement {
+    public static void solve() {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            // check whether n is big or a number
+            if (n > 100){
+                System.out.println("Big");
+            }
+            System.out.print("Number");
+    }
+
+    static void main() {
+        solve();
+    }
+}
--- src/Operator/Bitwise.java
+++ src/Operator/Bitwise.java
@@ -21,3 +21,9 @@ public class Bitwise {
     }
 }
 
+//Right Shift
+//public static void rightShift(int a, int b) {
+//    // code here
+//    System.out.println(a>>b);
+//}
+//}
