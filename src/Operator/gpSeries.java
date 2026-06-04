af48938a7d8219a82f31ccaeb1e8322a95a8fea2
Adil Jamal <jamal.adil7860@gmail.com>
2026-06-04 13:00:14 +0530

    gp series

---
+++ src/Operator/gpSeries.java
@@ -0,0 +1,12 @@
+package Operator;
+// User function Template for-Java
+class gpSeries {
+    public static void utility(int a, int r, int n) {
+        // Complete the code below
+        int ans = (int)(a * Math.pow(r, n-1));
+        // Complete the code above
+
+        // The line below prints the output. Don't change it!
+        System.out.println(ans);
+    }
+}
