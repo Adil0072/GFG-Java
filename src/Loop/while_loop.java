e8108291c7abb6f2f68f98085a154edf940c4110
Adil Jamal <jamal.adil7860@gmail.com>
2026-06-05 22:15:28 +0530

    while loop

--- src/Loop/while_loop.java
+++ src/Loop/while_loop.java
@@ -1,11 +1,19 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class while_loop {
-        public static void utility(int x) {
+        public static void dec() {
             // code here
+            Scanner in =  new Scanner(System.in);
+            int x = in.nextInt();
             while (x >= 0){
                 System.out.print(x + " ");
                 x--;
             }
         }
+
+    static void main() {
+        dec();
+    }
 }
