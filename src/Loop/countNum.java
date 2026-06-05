9057460fbc1fe7121c7a8c25622f8984079fd3bc
Adil Jamal <jamal.adil7860@gmail.com>
2026-06-05 22:36:15 +0530

    count number

---
+++ src/Loop/countNum.java
@@ -0,0 +1,29 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class countNum {
+    public static int countDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int count = 0;
+        while (n > 0) {
+            count++;
+            n /= 10;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        System.out.println(countDigits(n));
+
+        in.close();
+    }
+}
