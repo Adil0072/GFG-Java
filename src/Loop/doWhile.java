62fb39cb9ec3287df9b18e321ddc8688b85e315c
Adil Jamal <jamal.adil7860@gmail.com>
2026-06-05 22:21:51 +0530

    dowhile loop

---
+++ src/Loop/doWhile.java
@@ -0,0 +1,18 @@
+package Loop;
+import java.util.Scanner;
+class doWhile {
+    public static void printGeeks() {
+        // Code here
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int i = 0;
+        do{
+            System.out.println("Geeksforgeeks");
+            i++;
+        }while(i < n);
+    }
+
+    static void main() {
+        printGeeks();
+    }
+}
