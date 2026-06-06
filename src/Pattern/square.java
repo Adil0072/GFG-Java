59f4e67b8bc826e4ba40d568fbf64d383e0cb16a
Adil Jamal <jamal.adil7860@gmail.com>
2026-06-06 23:04:01 +0530

    pyramid

--- src/Pattern/square.java
+++ src/Pattern/square.java
@@ -2,7 +2,7 @@ package Pattern;
 import java.util.Scanner;
 
 class square {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
