da2e1e4437041e1167de5097bb95f148d6376660
Adil Jamal <jamal.adil7860@gmail.com>
2026-06-06 22:57:11 +0530

    square

---
+++ src/Pattern/square.java
@@ -0,0 +1,17 @@
+package Pattern;
+import java.util.Scanner;
+
+class square {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
